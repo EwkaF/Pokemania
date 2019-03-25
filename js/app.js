7c2ca92e1912ef519d913eb0ed3655c0c753d084
EwkaF <obarka@interia.pl>
2019-03-25 21:13:23 +0100

    formating done

--- js/app.js
+++ js/app.js
@@ -1,128 +1,122 @@
-$(function() {
-    
+$(function () {
+
     var url = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
-    var nextUrl = url+'/'+'?limit=10&offset=';
+    var nextUrl = url + '/' + '?limit=10&offset=';
     var evolutionUrl = 'https://pokeapi.co/api/v2/evolution-chain/1/';
     var offset = 0;
 
     $('.pokemon-details').hide();
 
-    function loadContent(){
+    function loadContent() {
 
-    function loadPagination(number){
-        $('#pagination').html('');
-        for (var i = 0; i < number.count/10; i++){
-            let paginationButton = $('<button></button>');
-            paginationButton.text(i);
-            paginationButton.addClass('paginationButton');
-            paginationButton.attr('id', i);
-            $('#pagination').append(paginationButton);
-            
+        function loadPagination(number) {
+            $('#pagination').html('');
+            for (var i = 0; i < number.count / 10; i++) {
+                let paginationButton = $('<button></button>');
+                paginationButton.text(i);
+                paginationButton.addClass('paginationButton');
+                paginationButton.attr('id', i);
+                $('#pagination').append(paginationButton);
+
+            }
+
         }
-        
-    }
 
-    function loadPokemon(pokemon){
-            
-       for( var i = 0; i < pokemon.length; i++){
+        function loadPokemon(pokemon) {
 
-            let id = 0;
-            if(offset === 0){
-                id = i+1;
+            for (var i = 0; i < pokemon.length; i++) {
+
+                let id = 0;
+                if (offset === 0) {
+                    id = i + 1;
+                }
+                else {
+                    id = i + 1 + offset;
+                }
+
+                let li = $('<li>');
+                li.addClass('row')
+                let name = $('<h3>, {class: "name"}');
+                name.addClass('col');
+                name.text("#" + (id) + ". " + pokemon[i].name);
+                let moreInfo = $('<button>Pokaż</button>');
+                moreInfo.addClass('showInfo col');
+                moreInfo.attr('id', id);
+                li.append(name);
+                li.append(moreInfo);
+                $('#pokemonList').append(li)
             }
-            else{
-                id = i + 1 + offset;
-            }
 
-            let li = $('<li>');
-            li.addClass('row')
-            let name = $('<h3>, {class: "name"}');
-            name.addClass('col');
-            name.text("#"+ (id) +". " + pokemon[i].name);
-            let moreInfo = $('<button>Pokaż</button>');
-            moreInfo.addClass('showInfo col');
-            moreInfo.attr('id', id);
-            li.append(name);
-            li.append(moreInfo);
-            $('#pokemonList').append(li)  
-       }
+            function loadPokemonDetails(onePokemon) {
 
-       
-       $('#pokemonList').on('click', '.showInfo', function(e){
-    
-           console.log("pokazuje wiecej");
-           console.log($(this)[0].id);
-            thisButton = $(this)
-
-            function loadPokemonDetails(onePokemon){
                 $('.name span').text("#" + onePokemon.id)
                 let h1 = $('.name strong');
                 h1.text(onePokemon.species.name);
                 let img = $('.pokemon-image');
                 img.attr("src", onePokemon.sprites.front_default);
-                let type =$('.type strong');
+                let type = $('.type strong');
                 let typ = "";
 
-                for (var i = 0; i < onePokemon.types.length; i++){
+                for (var i = 0; i < onePokemon.types.length; i++) {
                     let pokemonType = onePokemon.types[i].type.name
-                    switch (pokemonType){
+                    switch (pokemonType) {
                         case "grass":
-                        pokemonType = "trawiasty"
-                        break;
+                            pokemonType = "trawiasty"
+                            break;
                         case "poison":
-                        pokemonType = "trujący"
-                        break;
+                            pokemonType = "trujący"
+                            break;
                         case "fire":
-                        pokemonType = "ognisty"
-                        break;
+                            pokemonType = "ognisty"
+                            break;
                         case "flying":
-                        pokemonType = "powietrzny"
-                        break;
+                            pokemonType = "powietrzny"
+                            break;
                         case "water":
-                        pokemonType = "wodny"
-                        break;
+                            pokemonType = "wodny"
+                            break;
                         case "bug":
-                        pokemonType = "robak"
-                        break;
+                            pokemonType = "robak"
+                            break;
                         case "normal":
-                        pokemonType = "normalny"
-                        break;
+                            pokemonType = "normalny"
+                            break;
                         case "electric":
-                        pokemonType = "elektryczny"
-                        break;
+                            pokemonType = "elektryczny"
+                            break;
                         case "ground":
-                        pokemonType = "ziemny"
-                        break;
+                            pokemonType = "ziemny"
+                            break;
                         case "rock":
-                        pokemonType = "kamienny"
-                        break;
+                            pokemonType = "kamienny"
+                            break;
                         case "fairy":
-                        pokemonType = "wróżka"
-                        break;
+                            pokemonType = "wróżka"
+                            break;
                         case "fighting":
-                        pokemonType = "walczący"
-                        break;
+                            pokemonType = "walczący"
+                            break;
                         case "psychic":
-                        pokemonType = "psychiczny"
-                        break;
+                            pokemonType = "psychiczny"
+                            break;
                         case "steel":
-                        pokemonType = "stalowy"
-                        break;
+                            pokemonType = "stalowy"
+                            break;
                         case "ice":
-                        pokemonType = "lodowy"
-                        break;
+                            pokemonType = "lodowy"
+                            break;
                         case "ghost":
-                        pokemonType = "duch"
-                        break;
+                            pokemonType = "duch"
+                            break;
                         case "dragon":
-                        pokemonType = "smok"
-                        break;
+                            pokemonType = "smok"
+                            break;
                         case "dark":
-                        pokemonType = "mroczny"
-                        break;
+                            pokemonType = "mroczny"
+                            break;
 
                     }
-                    typ += pokemonType +' ';
+                    typ += pokemonType + ' ';
 
                 }
                 type.text(typ);
@@ -139,18 +133,18 @@ $(function() {
                 let speed = $('.speed strong');
                 speed.text(onePokemon.stats[0].base_stat);
 
-               $('.pokemon-details').show();
+                $('.pokemon-details').show();
 
-                $('.close').on('click', function(e){
+                $('.close').on('click', function (e) {
                     $('.pokemon-details').hide();
                 });
-                
+
 
             }
 
-            function loadError(){
+            function loadError() {
                 $('.name span').text("Brak danych");
-                $('.pokemon-image').attr('src',"logo.png");
+                $('.pokemon-image').attr('src', "logo.png");
                 $('.name strong').text("");
                 $('.type strong').text("")
                 $('.pokemon-details').show();
@@ -162,58 +156,61 @@ $(function() {
                 $('.speed strong').text("");
 
 
-                $('.close').on('click', function(e){
+                $('.close').on('click', function (e) {
                     $('.pokemon-details').hide();
                 });
             }
 
-            let pokemonId = thisButton[0].id
-           $.ajax({
-             url: 'https://pokeapi.co/api/v2/pokemon' +'/' + pokemonId
+            $('#pokemonList').on('click', '.showInfo', function (e) {
 
-           }).done(function(response){
-                loadPokemonDetails(response);
-           }).fail(function(message){
-               loadError()
-            // $('.name span').text("Brak danych");
-            // $('.name strong').text("");
-            // $('.pokemon-image').attr('src',"");
-            //    let noData = $('li');
-            //    noData.text("Brak danych");
-            //    $('.pokemon-details').append(noData)
-            console.log(message);
-            console.log("brak danych")
-            
-           })
-       })
+                console.log("pokazuje wiecej");
+                console.log($(this)[0].id);
+                // thisButton = $(this);
+                // let pokemonId = thisButton[0].id;
+
+                let pokemonId = $(this)[0].id;
+
+                $.ajax({
+                    url: 'https://pokeapi.co/api/v2/pokemon' + '/' + pokemonId
+
+                }).done(function (response) {
+                    console.log(response)
+                    loadPokemonDetails(response);
+                }).fail(function (message) {
+                    loadError()
+                    console.log(message);
+                    console.log("brak danych")
+
+                })
+            })
+        }
+
+
+        $.ajax({
+            url: url
+
+        }).done(function (response) {
+            $('#pokemonList').html("");
+            console.log(response.count)
+            loadPagination(response)
+            loadPokemon(response.results);
+        }).fail(function (message) {
+            console.log(message)
+        })
     }
 
 
-$.ajax({
-url: url
 
-}).done(function(response){
-    $('#pokemonList').html("");
-    console.log(response.count)
-    loadPagination(response)
-loadPokemon(response.results);
-}).fail(function(message){
-    console.log(message)
-})
-}
+    $('#pagination').on('click', '.paginationButton', function (e) {
+        console.log($(this).attr('id'));
+        offset = $(this).attr('id') * 10;
+        url = nextUrl + offset;
+        loadContent();
+
+
+    })
 
 
 
-$('#pagination').on('click','.paginationButton', function(e){
-    console.log($(this).attr('id'));
-    offset = $(this).attr('id') * 10;
-    url = nextUrl + offset;
     loadContent();
-
-
-})
-
-
-
-loadContent();
 })
